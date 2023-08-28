78ce3b87bf1965f61819046bc7a3079d9216f19c
maumassaro <70840029+maumassaro@users.noreply.github.com>
2023-08-28 18:57:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 import os
+import sqlite3
 
 # loading PDF, DOCX and TXT files as LangChain Documents
 def load_document(file):
